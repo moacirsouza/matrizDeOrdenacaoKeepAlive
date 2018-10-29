46cb1a611dd1b2cc94668cf69a4c10ed96642b68
moacirsouza <moacir.souza@gmail.com>
2018-10-29 15:51:57 -0300

    Implementação da lista usando arrays

--- moka.sh
+++ moka.sh
@@ -8,9 +8,31 @@ funcao(){
     # echo ${array[@]}
     # echo ${array[0]}
 
+    tamanhoDoArray=${#array[@]}
+
     for i in ${array[@]}
     do
-        echo "${array[@]:1:2}"
+        #echo "Número da iteração ${i}"
+        echo "Tamanho do Array: ${tamanhoDoArray}"
+        echo "Antes do unset: ${array[@]}"
+
+        ### Recebe o primeiro elemento do Array atual
+        topoDoArray=${array[$i-1]}
+
+        ### O unset remove o item da posição correta do array, mas
+        ### como efeito colateral, o índice do array também é
+        ### destruído, ...
+        unset array[$i-1]
+
+        ### ... por isso é preciso recriar o índice, embora vazio,
+        ### usando o set
+        # array[$i-1]="$topoDoArray" 
+        echo "Topo do Array: $topoDoArray"
+
+        # echo "Depois do unset: ${array[@]}"
+        array2=(${array[@]})
+        array[$tamanhoDoArray]=$topoDoArray
+        echo -e "Depois da reatribuição do array: ${array2[@]} \n"
     done
 }
 
